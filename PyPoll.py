9289407b022d1afaf332ff88d245a28df1f5240f
Katie Bernstein <katie.oestreich@gmail.com>
2022-06-23 22:24:29 -0700

    Updated PyPoll

--- PyPoll.py
+++ PyPoll.py
@@ -5,40 +5,49 @@
 # 4. The total number of votes each candidate won 
 # 5. The winner of the election based on popular vote
 
-#received os not found error - had to define
+# Add our dependencies
+import csv
 import os
 
 # Assign a variable for the file to load and the path.
-# Python not running in script directory full path used - relative path does not currently work
-
-file_to_load = '/Users/katiebernstein/Documents/Data Bootcamp/Git/Election_Analysis/Resources/election_results.csv'
+file_to_load = os.path.join("Resources", "election_results.csv")
 
 # Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("/Users/katiebernstein/Documents/Data Bootcamp/Git/Election_Analysis/analysis/", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Use the open statement to open the file as a text file.
-with open(file_to_save, "w") as txt_file:
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
 
-    # Write some data to the file.
-    txt_file.write("Hello World")
+    # To do: read and analyze the data here.
+
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
     
-    # Write three counties to the file.
-     txt_file.write("Arapahoe")
-     txt_file.write("Denver")
-     txt_file.write("Jefferson")
-
-# Close the file
-txt_file.close()
-
-# Using the open() function with the "w" mode we will write data to the file.
-open(file_to_save, "w")
-
-# Open the election results and read the file 
-with open(file_to_load,'r') as election_data:
-
-    # to do: perform analysis
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
+    
+    # To do: perform analysis.
     print(election_data)
 
-# close the file
+# Close the file.
 election_data.close()
 
+
+#Other Code from Module
+#print(file_to_save)
+# election_data = open(file_to_load, 'r') - old code
+#print(__file__)
+#txt_file.write("Arapahoe, ")
+#txt_file.write("Denver, ")
+#txt_file.write("Jefferson")
+# # Use the open statement to open the file as a text file.
+#with open(file_to_save, "w") as txt_file:
+
+    # Write three counties to the file.
+    #txt_file.write("Counties in the Election\n-----------------\nArapahoe\nDenver\nJefferson")
+    # Close the file
+    #txt_file.close()   
+    # Print each row in the CSV file.
+    #for row in file_reader:
+        #print(row)
